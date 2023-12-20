703bad588656b99c9de8a9ab45583d24d7af5a3b
piernov <piernov@piernov.org>
2023-12-20 16:05:40 +0100

    Quantizer: add exclude arg to quantize_weights

--- src/qualia_codegen_core/Quantizer.py
+++ src/qualia_codegen_core/Quantizer.py
@@ -4,6 +4,9 @@ import logging
 from typing import TYPE_CHECKING
 
 import numpy as np
+import numpy.typing
+
+from qualia_codegen_core.typing import NDArrayFloatOrInt
 
 if TYPE_CHECKING:
     from .graph.LayerNode import LayerNode
@@ -18,30 +21,43 @@ class Quantizer:
         self.number_min = -(2 ** (width - 1))
         self.number_max = 2 ** (width - 1) - 1
 
-    def quantize_weights_with_scale_factor(self, node: LayerNode, scale_factor: int) -> bool:
+    def quantize_array_with_scale_factor(self,
+                                         arr: NDArrayFloatOrInt,
+                                         scale_factor: int) -> NDArrayFloatOrInt | None:
         target_dtype = getattr(np, f'int{self.width}', None)
         if target_dtype is None: # Initialization failed due to unsupported width
             logger.error('No integer data type for width %s', self.width)
-            return False
+            return None
+
+        # Already integer, no need to quantize
+        if np.issubdtype(arr.dtype, np.integer):
+            return arr
+
+        new_arr = arr * (1 << scale_factor)
+        new_arr = np.floor(new_arr)
+        new_arr = np.clip(new_arr, self.number_min, self.number_max)
+        return new_arr.astype(target_dtype)
+
+    def quantize_weights_with_scale_factor(self, node: LayerNode, scale_factor: int, exclude: list[str] | None = None) -> bool:
 
         for weights_name, weights in node.layer.weights.items():
-            # Already integer, no need to quantize
-            if np.issubdtype(weights.dtype, np.integer):
+            # Skip excluded weights
+            if exclude and weights_name in exclude:
                 continue
 
-            new_weights = weights * (1 << scale_factor)
-            new_weights = np.floor(new_weights)
-            new_weights = np.clip(new_weights, self.number_min, self.number_max)
-            new_weights = new_weights.astype(target_dtype)
+            new_weights = self.quantize_array_with_scale_factor(weights, scale_factor=scale_factor)
+            if new_weights is None:
+                return False
 
             setattr(node.layer, weights_name, new_weights)
+
         return True
 
-    def quantize_weights(self, node: LayerNode) -> bool:
+    def quantize_weights(self, node: LayerNode, exclude: list[str] | None = None) -> bool:
         if len(node.layer.weights) > 0:
             if node.q.weights_scale_factor is None:
                 logger.error('No weights quantization information for %s', node.layer.name)
                 return False
             logger.info('%s quantization weights=%s', node.layer.name, node.q.weights_scale_factor)
-            return self.quantize_weights_with_scale_factor(node, node.q.weights_scale_factor)
+            return self.quantize_weights_with_scale_factor(node, node.q.weights_scale_factor, exclude=exclude)
         return True
