103cbf90ccbd988c3545fe856c76819b7c13356e
piernov <piernov@piernov.org>
2023-12-22 15:15:50 +0100

    Quantizer: quantize weights according to round mode

--- src/qualia_codegen_core/Quantizer.py
+++ src/qualia_codegen_core/Quantizer.py
@@ -4,6 +4,7 @@ import logging
 
 import numpy as np
 
+from qualia_codegen_core.graph.RoundMode import RoundMode
 from qualia_codegen_core.typing import TYPE_CHECKING, NDArrayFloatOrInt
 
 if TYPE_CHECKING:
@@ -21,7 +22,8 @@ class Quantizer:
 
     def quantize_array_with_scale_factor(self,
                                          arr: NDArrayFloatOrInt,
-                                         scale_factor: int) -> NDArrayFloatOrInt | None:
+                                         scale_factor: int,
+                                         round_mode: RoundMode) -> NDArrayFloatOrInt | None:
         target_dtype = getattr(np, f'int{self.width}', None)
         if target_dtype is None: # Initialization failed due to unsupported width
             logger.error('No integer data type for width %s', self.width)
@@ -32,18 +34,29 @@ class Quantizer:
             return arr
 
         new_arr = arr * (1 << scale_factor)
-        new_arr = np.floor(new_arr)
+        if round_mode == RoundMode.FLOOR:
+            new_arr = np.floor(new_arr)
+        elif round_mode == RoundMode.NEAREST:
+            new_arr = np.floor(new_arr + 0.5)
+        else:
+            logger.error('Unsupported round mode: %s, supported: floor, nearest', round_mode)
+            raise ValueError
+
         new_arr = np.clip(new_arr, self.number_min, self.number_max)
         return new_arr.astype(target_dtype)
 
-    def quantize_weights_with_scale_factor(self, node: LayerNode, scale_factor: int, exclude: list[str] | None = None) -> bool:
+    def quantize_weights_with_scale_factor(self,
+                                           node: LayerNode,
+                                           scale_factor: int,
+                                           round_mode: RoundMode,
+                                           exclude: list[str] | None = None) -> bool:
 
         for weights_name, weights in node.layer.weights.items():
             # Skip excluded weights
             if exclude and weights_name in exclude:
                 continue
 
-            new_weights = self.quantize_array_with_scale_factor(weights, scale_factor=scale_factor)
+            new_weights = self.quantize_array_with_scale_factor(weights, scale_factor=scale_factor, round_mode=round_mode)
             if new_weights is None:
                 return False
 
@@ -56,6 +69,14 @@ class Quantizer:
             if node.q.weights_scale_factor is None:
                 logger.error('No weights quantization information for %s', node.layer.name)
                 return False
+
+            if node.q.weights_round_mode is None:
+                logger.error('No round mode select for %s', node.layer.name)
+                return False
+
             logger.info('%s quantization weights=%s', node.layer.name, node.q.weights_scale_factor)
-            return self.quantize_weights_with_scale_factor(node, node.q.weights_scale_factor, exclude=exclude)
+            return self.quantize_weights_with_scale_factor(node,
+                                                           node.q.weights_scale_factor,
+                                                           round_mode=node.q.weights_round_mode,
+                                                           exclude=exclude)
         return True
