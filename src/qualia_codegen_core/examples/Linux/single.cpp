cf2559b7769a1ac7391bf28b1cf799abc97d42cb
piernov <piernov@piernov.org>
2023-12-22 15:43:02 +0100

    examples/Linux/single: Quantize input according to round mode

--- src/qualia_codegen_core/examples/Linux/single.cpp
+++ src/qualia_codegen_core/examples/Linux/single.cpp
@@ -8,6 +8,16 @@
 //#include "output/number.h"
 #include "model.h"
 
+static inline float round_with_mode(float v, round_mode_t round_mode) {
+	if (round_mode == ROUND_MODE_FLOOR) {
+		return floorf(v);
+	} else if (round_mode == ROUND_MODE_NEAREST) {
+		return floorf(v + 0.5f);
+	} else {
+		return v;
+	}
+}
+
 int main(int argc, const char*argv[]) {
 	if (argc < 2) {
 		printf("Usage: %s <test vector>\n", argv[0]);
@@ -22,7 +32,7 @@ int main(int argc, const char*argv[]) {
 	for (size_t i = 0; i < MODEL_INPUT_DIMS; i++) {
 		// Fixed-point conversion if model input is integer
 		if constexpr(std::is_integral_v<MODEL_INPUT_NUMBER_T>) {
-			input_flat[i] = clamp_to(MODEL_INPUT_NUMBER_T, (MODEL_INPUT_LONG_NUMBER_T)floor(strtof(argv[i + 1], NULL) * (1<<MODEL_INPUT_SCALE_FACTOR)));
+			input_flat[i] = clamp_to(MODEL_INPUT_NUMBER_T, (MODEL_INPUT_LONG_NUMBER_T)round_with_mode(strtof(argv[i + 1], NULL) * (1<<MODEL_INPUT_SCALE_FACTOR)));
 		} else {
 			input_flat[i] = strtof(argv[i + 1], NULL);
 		}
