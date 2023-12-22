5f7bac4e6990ec33d06c0929f45d349a418baf63
piernov <piernov@piernov.org>
2023-12-22 15:13:39 +0100

    Add graph.ActivationRange

---
+++ src/qualia_codegen_core/graph/ActivationRange.py
@@ -0,0 +1,17 @@
+from __future__ import annotations
+
+from dataclasses import dataclass
+
+from qualia_codegen_core.typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from qualia_codegen_core.graph.RoundMode import RoundMode  # noqa: TCH001
+
+@dataclass
+class ActivationRange:
+    input_q: int | None
+    activation_q: int | None
+    weights_q: int | None
+    input_round_mode: RoundMode | None
+    activation_round_mode: RoundMode | None
+    weights_round_mode: RoundMode | None
