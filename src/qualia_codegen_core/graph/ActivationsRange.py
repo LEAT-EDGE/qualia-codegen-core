bbbf0fc55c4f9251976771e022070a7edf301e3b
piernov <piernov@piernov.org>
2024-01-17 18:08:13 +0100

    main.py/ActivationsRange: read bias_scale_factor from activation_ranges.txt

--- src/qualia_codegen_core/graph/ActivationsRange.py
+++ src/qualia_codegen_core/graph/ActivationsRange.py
@@ -29,11 +29,12 @@ class ActivationsRange(dict[str, ActivationRange]):
             for line in f:
                 r = line.strip().split(',')
                 self[r[0]] = ActivationRange(self.__int_or_none(r[1]),
-                                                          self.__int_or_none(r[2]),
-                                                          self.__int_or_none(r[3]),
-                                                          self.__roundmode_or_none(r[4]),
-                                                          self.__roundmode_or_none(r[5]),
-                                                          self.__roundmode_or_none(r[6]))
+                                             self.__int_or_none(r[2]),
+                                             self.__int_or_none(r[3]),
+                                             self.__int_or_none(r[4]),
+                                             self.__roundmode_or_none(r[5]),
+                                             self.__roundmode_or_none(r[6]),
+                                             self.__roundmode_or_none(r[7]))
                 if first_input_q is None:
                     first_input_q = self.__int_or_none(r[1])
                 if first_input_round_mode is None:
@@ -41,9 +42,10 @@ class ActivationsRange(dict[str, ActivationRange]):
 
         # Model input range
         self[input_layer_name] = ActivationRange(first_input_q,
-                                                              first_input_q,
-                                                              0,
-                                                              first_input_round_mode,
-                                                              first_input_round_mode,
-                                                              None)
+                                                 first_input_q,
+                                                 0,
+                                                 None,
+                                                 first_input_round_mode,
+                                                 first_input_round_mode,
+                                                 None)
         return self
