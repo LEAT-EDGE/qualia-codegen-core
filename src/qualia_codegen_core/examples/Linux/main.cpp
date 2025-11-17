5afe6d8598008938f77b5bcc123a1f0f0d380fc3
piernov <piernov@piernov.org>
2025-11-17 17:45:03 +0100

    examples/Linux/main.cpp: optionally save predictions to file

--- src/qualia_codegen_core/examples/Linux/main.cpp
+++ src/qualia_codegen_core/examples/Linux/main.cpp
@@ -5,6 +5,7 @@
 #include <fstream>
 #include <iostream>
 #include <memory>
+#include <optional>
 #include <sstream>
 #include <fstream> 
 #include <vector>
@@ -30,13 +31,31 @@ std::vector<std::array<float, N>> readInputsFromFile(const char *filename) {
 	return inputs;
 }
 
+template<typename T, size_t OutputDims>
+void writePredsToFile(const std::array<T, OutputDims> &preds, std::ofstream &fout) {
+	for (size_t j = 0; j < preds.size(); j++) {
+		if (j != 0) {
+			fout << ',';
+		}
+		fout << preds[j];
+	}
+	fout << std::endl;
+}
+
 //Compute testing accuracy
 template<size_t InputDims, size_t OutputDims>
-void evaluate(const std::vector<std::array<float, InputDims>> &inputs, const std::vector<std::array<float, OutputDims>> &targets) {
+void evaluate(
+		const std::vector<std::array<float, InputDims>> &inputs,
+		const std::vector<std::array<float, OutputDims>> &targets,
+		std::optional<std::ofstream> &fout) {
 	static NeuralNetwork nn{metrics};
 
 	for (size_t i = 0;  i < inputs.size() ; i++){ //&& i < labels.size()-1000; i++) {
-		nn.evaluate(inputs.at(i), targets.at(i));
+		auto preds = nn.evaluate(inputs.at(i), targets.at(i));
+
+		if (fout) {
+			writePredsToFile(preds, fout.value());
+		}
 	}
 
 	auto metrics_result = nn.getMetricsResult();
@@ -47,15 +66,20 @@ void evaluate(const std::vector<std::array<float, InputDims>> &inputs, const std
 }
 
 int main(int argc, const char *argv[]) {
-	if (argc != 3) {
-		std::cerr << "Usage: " << argv[0] << " testX.csv testY.csv" << std::endl;
+	if (argc < 3) {
+		std::cerr << "Usage: " << argv[0] << " test_x.csv test_y.csv [preds.csv]" << std::endl;
 		exit(1);
 	}
 
 	auto inputs = readInputsFromFile<MODEL_INPUT_DIMS>(argv[1]);
 	auto labels = readInputsFromFile<MODEL_OUTPUT_SAMPLES>(argv[2]);
 
-	evaluate(inputs, labels);
+	std::optional<std::ofstream> fout;
+	if (argc == 4) {
+		fout = std::ofstream{argv[3]};
+	}
+
+	evaluate(inputs, labels, fout);
 
 	return 0;
 }
