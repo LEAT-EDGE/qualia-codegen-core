206ffe28e60980b82730a4d77c318fbecb1bbfcf
piernov <piernov@piernov.org>
2026-02-05 14:18:05 +0100

    main: populated ActivationRange _bits fields

--- src/qualia_codegen_core/graph/ActivationsRange.py
+++ src/qualia_codegen_core/graph/ActivationsRange.py
@@ -29,17 +29,17 @@ class ActivationsRange(dict[str, ActivationRange]):
         with path.open() as f:
             for line in f:
                 r = line.strip().split(',')
-                self[r[0]] = ActivationRange(self.__int_or_none(r[1]),
-                                             self.__int_or_none(r[2]),
-                                             self.__int_or_none(r[3]),
-                                             self.__int_or_none(r[4]),
-                                             self.__int_or_none(r[5]),
-                                             self.__int_or_none(r[6]),
-                                             self.__int_or_none(r[7]),
-                                             self.__int_or_none(r[8]),
-                                             self.__roundmode_or_none(r[9]),
-                                             self.__roundmode_or_none(r[10]),
-                                             self.__roundmode_or_none(r[11]))
+                self[r[0]] = ActivationRange(self.__int_or_none(r[1]),  # input_bits
+                                             self.__int_or_none(r[2]),  # activation_bits
+                                             self.__int_or_none(r[3]),  # weights_bits
+                                             self.__int_or_none(r[4]),  # bias_bits
+                                             self.__int_or_none(r[5]),  # input_q
+                                             self.__int_or_none(r[6]),  # activation_q
+                                             self.__int_or_none(r[7]),  # weights_q
+                                             self.__int_or_none(r[8]),  # bias_q
+                                             self.__roundmode_or_none(r[9]),  # input_round_mode
+                                             self.__roundmode_or_none(r[10]),  # activation_round_mode
+                                             self.__roundmode_or_none(r[11]))  # weights_round_mode
                 if first_input_bits is None:
                     first_input_bits = self.__int_or_none(r[1])
                 if first_input_q is None:
@@ -48,15 +48,15 @@ class ActivationsRange(dict[str, ActivationRange]):
                     first_input_round_mode = self[r[0]].input_round_mode
 
         # Model input range
-        self[input_layer_name] = ActivationRange(first_input_bits,
-                                                 first_input_bits,
-                                                 0,
-                                                 None,
-                                                 first_input_q,
-                                                 first_input_q,
-                                                 0,
-                                                 None,
-                                                 first_input_round_mode,
-                                                 first_input_round_mode,
-                                                 None)
+        self[input_layer_name] = ActivationRange(first_input_bits,  # input_bits
+                                                 first_input_bits,  # activation_bits
+                                                 0,  # weights_bits
+                                                 None,  # bias_bits
+                                                 first_input_q,  # input_q
+                                                 first_input_q,  # activation_q
+                                                 0,  # weights_q
+                                                 None,  # bias_q
+                                                 first_input_round_mode,  # input_round_mode
+                                                 first_input_round_mode,  # activation_round_mode
+                                                 None)  # weights_round_mode
         return self
